40b92d6905d5014b7ed7b6df7f623a0656007458
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-06-08 03:29:32 +0900

    Client Setting test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  username: string;
+}
 
 function App() {
-  return <h1>hi</h1>;
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch('/user'); // 서버 URL 확인
+        const data: User[] = await response.json();
+
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    }
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Users</h1>
+      {users.map(user => (
+        <div key={user.id}>{user.username}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
