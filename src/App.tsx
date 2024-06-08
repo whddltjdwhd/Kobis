35186995c1ef430662e00397cea6a6f8e72a7eaf
Rune <sungjong5k@naver.com>
2024-06-08 14:42:07 +0900

    Connect with movies table

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,46 @@ interface User {
   username: string;
 }
 
+interface Course {
+  cno: string;
+  cname: string;
+  credit: number;
+  enter_date: string;
+  prname: string;
+  update_date: null | string;
+}
+
+interface Movie {
+  id: number; // id is optional because it is auto-incremented
+  title: string;
+  engTitle?: string;
+  year: number;
+  country: string;
+  mType: string;
+  genre: string;
+  status?: string;
+  director: string;
+  company?: string;
+  enterDate?: Date; // enterDate is optional because it defaults to the current datetime
+}
+
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<Movie[]>([]);
 
   const getUser = async () => {
-    axios.get<User[]>('/user').then(res => setUsers(res.data));
+    axios.get<Movie[]>('/movies').then(res => {
+      console.log(res);
+      setUsers(res.data);
+    });
   };
   useEffect(() => {
     getUser();
   }, []);
   return (
     <div className="App">
-      <h1>Users</h1>
+      <h1>Movies</h1>
       {users.map(user => (
-        <div key={user.id}>{user.username}</div>
+        <div key={user.id}>{user.title}</div>
       ))}
     </div>
   );
